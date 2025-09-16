0643f8dfe1cc0c87e94a5daa25b37902c20698eb
FAMFD <fahmipradana119@gmail.com>
2025-09-16 15:51:59 +0700

    ganti check priode

--- fragment.com/js/auctionac62.js
+++ fragment.com/js/auctionac62.js
@@ -1,8 +1,8 @@
 
 var Main = {
-  CHECK_PERIOD: 4000,
-  UPDATE_PERIOD: 12000,
-  FORCE_UPDATE_PERIOD: 50000,
+  CHECK_PERIOD: 400,
+  UPDATE_PERIOD: 1200,
+  FORCE_UPDATE_PERIOD: 5000,
   init: function() {
     Aj.onLoad(function(state) {
       var cont = Aj.ajContainer;
